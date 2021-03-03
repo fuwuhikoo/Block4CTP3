9b9915e5d9ad225319d49c6cc8988cfb2a6d5d0d
fuwuhikoo <79867004+fuwuhikoo@users.noreply.github.com>
2021-03-03 10:09:41 -0800

    Add files via upload

---
+++ flan3.3.21.py
@@ -0,0 +1,38 @@
+"""
+March 2, 2021
+
+Use this document to record your answers to the Parson's Problems
+shared in class.
+
+Remember: the idea behind a Parson's Problem is that you are
+shown completely correct code that is presented OUT OF ORDER.
+When arranged correctly, the code does what the Parson's Problem
+says it should do.
+
+"""
+
+
+#Write your answer to Parson's Problem #1 below:
+
+for x in range (o,10)
+print("hello world")
+#basic for loop
+
+#Write your answer to Parson's Problem #2 below:
+
+#basic list 
+myList = []
+myList.append("apples")
+myList.append("pinapples")
+print(myList)
+
+
+#Write your answer to Parson's Problem #3 below:
+
+#basic conditional
+x = input("Type a number")
+x = int(x)
+if x > 0:
+    print("That's a small number!")
+else:
+    print("That're more than nothing!")
